f9a29b4838ee8c049da508d4aebb9f662ffe38fe
Tes Gungaadorj <tesgungaadorj@Tess-MacBook-Pro.local>
2021-08-29 09:52:05 -0400

    Change in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println ("This is Tessa's project");
+        System.out.println ("Hello World");
 
     }
+    public static String revStr(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    // git status
+    // git add <the file we changed> - put into staging area
+    // git commit -m "your msg"
+    // git push
 }
